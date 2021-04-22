84749ed19390a23548a6605ba6ed3923d36f4afe
ItsEvdok <78257029+ItsEvdok@users.noreply.github.com>
2021-04-21 23:40:21 -0400

    Update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+// const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -57,12 +58,15 @@ const questions = [
 
 
 // // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {
+//     fs.writeToFile(fileName, data);
+// }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then(answers => {
-        console.log(answers);
+    inquirer.prompt(questions).then(data => {
+        console.log(data);
+        console.log('this is working');
     });
 }
 
